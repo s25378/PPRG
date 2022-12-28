f0f0f4d400cfeb197e8d7f54c9caf573c6c9ba58
s25378 <93827120+s25378@users.noreply.github.com>
2022-12-28 14:27:28 +0100

    Add files via upload

---
+++ z6.1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+
+
+using namespace std;
+
+int podzielna(int n);
+
+int main()
+{
+    int n;
+    cout << "Podaj zakres do ktorego obliczyc liczby podzielne przez 3 ale nie przez 5: ";
+    cin >> n;
+    cout << "Ilosc liczb to: " << podzielna(n);
+    return 0;
+}
+
+int podzielna(int n) {
+    int ilosc_liczb = 0;
+    for (int i = 0; i <= n; i++) {
+        if (i % 5 == 0 && i % 3 != 0) {
+            ilosc_liczb++;
+        }
+    }
+    return ilosc_liczb;
+}
+
+
+

4e01d6648527130958a1e330605e5ee8039a0201
s25378 <93827120+s25378@users.noreply.github.com>
2022-12-28 13:12:10 +0100

    update - skonczone

--- z5.2.cpp
+++ z5.2.cpp
@@ -7,46 +7,67 @@ using namespace std;
 int main()
 {
     int szerokosc, wysokosc;
-    int a=1;
-    
-    cout << "Podaj wysokosc tablicy: ";
-    cin >> wysokosc;
+    int a = 1;
+    int zm1, zm2;
+
     cout << "Podaj szerokosc tablicy: ";
+    cin >> wysokosc;
+    cout << "Podaj wysokosc tablicy: ";
     cin >> szerokosc;
     int default_value = 0;
     vector<int> v(szerokosc, default_value);
     vector<vector<int>> tablica(wysokosc, v);
-    
+
     for (int i = 0; i < szerokosc; i++) {
         for (int j = 0; j < wysokosc; j++) {
-            tablica[j][i]=a;
+            tablica[j][i] = a;
             a++;
         }
         cout << endl;
     }
-
-  
     
+    cout << "Podaj elementy tablicy: " << endl;
+
+    for (int i = 0; i < szerokosc; i++) {
+        for (int j = 0; j < wysokosc; j++) {
+            cin >> tablica[j][i];
+        }
+        //cout << endl;
+    }
+    
+
+
+    cout << endl;
     for (int i = 0; i < szerokosc; i++) {
         for (int j = 0; j < wysokosc; j++) {
             cout << tablica[j][i] << "  ";
         }
         cout << endl;
     }
+
     
-    /*
-    cout << "Podaj elementy tablicy: " << endl;
-    for (int i = 0; i < liczba_elementow; i++) {
-        cin >> a;
-        basic.push_back(a);
-        sorted.push_back(a);
+
+    for (int i = 0; i < szerokosc; i++) {
+        for (int j = 0; j < wysokosc/2; j++) {
+            zm1 = tablica[j][i];
+            zm2 = tablica[wysokosc - j - 1][i];
+            tablica[j][i] = zm2;
+            tablica[wysokosc - j - 1][i] = zm1;
+        }
+        //cout << endl;
     }
-    */
-    
+    cout << endl;
+    for (int i = 0; i < szerokosc; i++) {
+        for (int j = 0; j < wysokosc; j++) {
+            cout << tablica[j][i] << "  ";
+        }
+        cout << endl;
+    }
+
+
 
     return 0;
 }
-
 
 
 

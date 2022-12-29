8e81fac535c0cbf9099d9c16253bd59435be1c4a
s25378 <93827120+s25378@users.noreply.github.com>
2022-12-29 19:03:19 +0100

    Update z2.1.cpp
    poprawione

--- z2.1.cpp
+++ z2.1.cpp
@@ -9,11 +9,12 @@ int main()
     int zakres, i, j;
     int suma_czesc = 0;
     int suma_calk = 0;
-    
+
     cout << "Podaj zakres: ";
     cin >> zakres;
-    for (i = 0; i <= zakres; i++) {
-        for (j = 0; j < i; j++) {
+    for (i = 1; i <= zakres; i++) {
+        suma_czesc = 0;
+        for (j = 1; j <= i; j++) {
             suma_czesc += j;
         }
         suma_calk += suma_czesc;
@@ -23,4 +24,3 @@ int main()
 
     return 0;
 }
-

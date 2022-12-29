8f5f27aeeb8e1207346f927ac9e5f32354454389
s25378 <93827120+s25378@users.noreply.github.com>
2022-12-29 15:54:44 +0100

    6.4 added

---
+++ z6.4.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+string konkatencja(int n, string slowo);
+
+int main()
+{
+    int n;
+    string slowo;
+
+    cout << "Podaj slowo: ";
+    getline(cin, slowo);
+    cout << "Podaj liczbe konkatencji slowa: "; 
+    cin >> n;
+
+   cout << konkatencja(n, slowo);
+
+    return 0;
+}
+
+string konkatencja(int n, string slowo) {
+    string pom1 = slowo;
+    for (int i = 0; i < n; i++) {
+        slowo = slowo + pom1;
+    }
+    return slowo;
+}
+
+
+
+
+
+
+
